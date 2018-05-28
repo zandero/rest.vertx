ad11432dcbea340a64f637bf1128b71e21ce6e25
drejc <andrej@zavrsnik.si>
2018-05-28 22:15:24 +0200

    Introducing @Header annotation to add static headers (writers, rest endpoints)

---
+++ src/main/java/com/zandero/rest/annotation/Header.java
@@ -0,0 +1,21 @@
+package com.zandero.rest.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ *
+ */
+@Inherited
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Header {
+
+	/**
+	 * Name value header pair to be added to output
+	 * Can be applied to RESTs or response writers
+	 *
+	 * example: "X-Auth: test", or "X-Auth test" ... will add a "X-Auth" header with value "test"
+	 */
+	String[] value() default "";
+}
--- src/main/java/com/zandero/rest/writer/HttpResponseWriter.java
+++ src/main/java/com/zandero/rest/writer/HttpResponseWriter.java
@@ -1,5 +1,6 @@
 package com.zandero.rest.writer;
 
+import com.zandero.rest.annotation.Header;
 import com.zandero.rest.data.MediaTypeHelper;
 import com.zandero.rest.data.RouteDefinition;
 import io.vertx.core.http.HttpHeaders;
@@ -8,6 +9,8 @@ import io.vertx.core.http.HttpServerResponse;
 
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Response writer interface to implement
@@ -23,21 +26,66 @@ public interface HttpResponseWriter<T> {
 		if (!response.ended() &&
 		    !response.headers().contains(HttpHeaders.CONTENT_TYPE)) {
 
+			boolean addWildcardContentType = true;
+
+			// add static headers if any
+			Header headers = this.getClass().getAnnotation(Header.class);
+			Map<String, String> nameValuePairs = getNameValuePairs(headers != null ? headers.value() : null);
+			if (nameValuePairs != null && nameValuePairs.size() > 0) {
+				response.headers().addAll(nameValuePairs);
+
+				addWildcardContentType = !nameValuePairs.containsKey(HttpHeaders.CONTENT_TYPE.toString());
+			}
+
 			Produces writerProduces = this.getClass().getAnnotation(Produces.class);
 			if (writerProduces != null && writerProduces.value().length > 0) {
+
+				addWildcardContentType = false;
 				for (String produces : writerProduces.value()) {
 					response.putHeader(HttpHeaders.CONTENT_TYPE, produces);
 				}
 			} else {
 				if (definition != null &&
 				    definition.getProduces() != null) {
+
+					addWildcardContentType = false;
 					for (MediaType produces : definition.getProduces()) {
 						response.putHeader(HttpHeaders.CONTENT_TYPE, MediaTypeHelper.toString(produces));
 					}
-				} else {
-					response.putHeader(HttpHeaders.CONTENT_TYPE, MediaType.WILDCARD);
 				}
 			}
+
+			if (addWildcardContentType) {
+				response.putHeader(HttpHeaders.CONTENT_TYPE, MediaType.WILDCARD);
+			}
 		}
+	}
+
+	default Map<String, String> getNameValuePairs(String[] values) {
+		if (values == null || values.length == 0) {
+			return null;
+		}
+
+		Map<String, String> output = new HashMap<>();
+		for (String item : values) {
+
+			// default if split point can not be found
+			String name = item;
+			String value = "";
+
+			int idx = item.indexOf(":");
+			if (idx <= 0) {
+				idx = item.indexOf(" ");
+			}
+
+			if (idx > 0) {
+				name = item.substring(0, idx).trim();
+				value = item.substring(idx + 1).trim();
+			}
+
+			output.put(name, value);
+		}
+
+		return output;
 	}
 }
