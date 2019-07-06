a0901fb8c7277572d3a69bd7025960e2d81b2589
bojanlet <bojan.letnik@gmail.com>
2019-07-06 17:52:49 +0200

    Migrating injection test to JUnit5

--- src/test/java/com/zandero/rest/injection/test/GuicedInjectionTest.java
+++ src/test/java/com/zandero/rest/injection/test/GuicedInjectionTest.java
@@ -1,99 +1,78 @@
 package com.zandero.rest.injection.test;
-/*
+
 
 import com.zandero.rest.RestBuilder;
 import com.zandero.rest.VertxTest;
 import com.zandero.rest.injection.GuiceInjectionProvider;
 import com.zandero.rest.injection.GuicedRest;
-import com.zandero.rest.injection.InjectionProvider;
 import com.zandero.rest.test.json.Dummy;
 import com.zandero.utils.extra.JsonUtils;
-import io.vertx.ext.unit.Async;
-import io.vertx.ext.unit.VertxTestContext;
-import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.ext.web.Router;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import io.vertx.ext.web.codec.BodyCodec;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-*/
-/**
- *
- *//*
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(VertxExtension.class)
-public class GuicedInjectionTest extends VertxTest {
+class GuicedInjectionTest extends VertxTest {
 
-	public void startWith(InjectionProvider provider, VertxTestContext context) {
+    @BeforeAll
+    static void start() {
 
-before();
+        before();
 
-		Router router = new RestBuilder(vertx)
-			                .injectWith(provider)
-			                .register(GuicedRest.class)
-			                .build();
+        Router router = new RestBuilder(vertx)
+                .injectWith(new GuiceInjectionProvider())
+                .register(GuicedRest.class)
+                .build();
 
-		vertx.createHttpServer()
-		     .requestHandler(router)
-		     .listen(PORT);
-	}
+        vertx.createHttpServer()
+                .requestHandler(router)
+                .listen(PORT);
+    }
 
-	@Test
-	public void guiceCallInjectedRestTest(VertxTestContext context) {
+    @Test
+    void guiceCallInjectedRestTest(VertxTestContext context) {
 
-		startWith(new GuiceInjectionProvider(), context);
-
-		final Async async = context.async();
-
-		client.get(PORT, HOST, "/guice/A").as(BodyCodec.string())
+        client.get(PORT, HOST, "/guice/A")
+                .as(BodyCodec.string())
                 .send(context.succeeding(response -> context.verify(() -> {
+                    assertEquals(200, response.statusCode());
+                    assertEquals("1=I'm so dummy!", response.body());
+                    context.completeNow();
+                })));
+    }
 
-			assertEquals(200, response.statusCode());
+    @Test
+    void guiceCallGuicedRestTest(VertxTestContext context) {
 
-			response.bodyHandler(bodyHandler -> {
-				String body = bodyHandler.getString(0, bodyHandler.length());
-				assertEquals("1=I'm so dummy!", body);
-				context.completeNow();
-			})));
-	}
-
-	@Test
-	public void guiceCallGuicedRestTest(VertxTestContext context) {
-
-		startWith(new GuiceInjectionProvider(), context);
-
-		final Async async = context.async();
-
-		client.get(PORT, HOST, "/guiceit").as(BodyCodec.string())
+        client.get(PORT, HOST, "/guiceit")
+                .as(BodyCodec.string())
                 .send(context.succeeding(response -> context.verify(() -> {
+                    assertEquals(200, response.statusCode());
+                    assertEquals("Oh yes I'm so dummy!", response.body());
+                    context.completeNow();
+                })));
+    }
 
-			assertEquals(200, response.statusCode());
+    @Test
+    void guiceCallInjectedPost(VertxTestContext context) {
 
-			response.bodyHandler(bodyHandler -> {
-				String body = bodyHandler.toString();
-				assertEquals("Oh yes I'm so dummy!", body);
-				context.completeNow();
-			})));
-	}
-
-	@Test
-	public void guiceCallInjectedPost(VertxTestContext context) {
-
-		startWith(new GuiceInjectionProvider(), context);
-
-		final Async async = context.async();
-
-		Dummy json = new Dummy("test", "me");
-		client.post("/guice/json").as(BodyCodec.string())
-                .send(context.succeeding(response -> context.verify(() -> {
-
-			assertEquals(200, response.statusCode());
-
-			response.bodyHandler(bodyHandler -> {
-				String body = bodyHandler.getString(0, bodyHandler.length());
-				assertEquals("{\"name\":\"Received-Oh yes I'm so dummy!\",\"value\":\"Received-{\\\"name\\\":\\\"test\\\",\\\"value\\\":\\\"me\\\"}\"}", body);
-				context.completeNow();
-			});
-		}).putHeader("Content-Type", "application/json").end(JsonUtils.toJson(json));
-	}
+        Dummy json = new Dummy("test", "me");
+        client.post(PORT, HOST, "/guice/json")
+                .as(BodyCodec.string())
+                .putHeader("Content-Type", "application/json")
+                .sendBuffer(Buffer.buffer(JsonUtils.toJson(json)),
+                        context.succeeding(response -> context.verify(() -> {
+                            assertEquals(200, response.statusCode());
+                            assertEquals("{\"name\":\"Received-Oh yes I'm so dummy!\",\"value\":\"Received-{\\\"name\\\":\\\"test\\\",\\\"value\\\":\\\"me\\\"}\"}", response.body());
+                            context.completeNow();
+                        })));
+    }
 }
-*/
+
