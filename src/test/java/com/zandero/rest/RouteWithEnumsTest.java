466c5683f761339e7064096f3e09faad32a71b17
drejc <andrej@zavrsnik.si>
2020-09-11 15:10:12 +0200

    Added test with enum reader example

--- src/test/java/com/zandero/rest/RouteWithEnumsTest.java
+++ src/test/java/com/zandero/rest/RouteWithEnumsTest.java
@@ -1,6 +1,8 @@
 package com.zandero.rest;
 
+import com.zandero.rest.reader.MyEnumReader;
 import com.zandero.rest.test.TestEnumRest;
+import com.zandero.rest.test.data.MyEnum;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.codec.BodyCodec;
 import io.vertx.junit5.VertxExtension;
@@ -19,31 +21,47 @@ public class RouteWithEnumsTest extends VertxTest {
 
         before();
 
-        Router router = RestRouter.register(vertx, TestEnumRest.class);
+        Router router = new RestBuilder(vertx)
+                            .register(TestEnumRest.class)
+                            .reader(MyEnum.class, MyEnumReader.class)
+                            .build();
+
         vertx.createHttpServer()
-                .requestHandler(router)
-                .listen(PORT);
+            .requestHandler(router)
+            .listen(PORT);
     }
 
     @Test
     void valueOfTest(VertxTestContext context) {
 
         client.get(PORT, HOST, "/enum/simple/one").as(BodyCodec.string())
-                .send(context.succeeding(response -> context.verify(() -> {
-                    assertEquals("one", response.body());
-                    assertEquals(200, response.statusCode());
-                    context.completeNow();
-                })));
+            .send(context.succeeding(response -> context.verify(() -> {
+                assertEquals("one", response.body());
+                assertEquals(200, response.statusCode());
+                context.completeNow();
+            })));
     }
+
+    @Test
+    void valueOfReader(VertxTestContext context) {
+
+        client.get(PORT, HOST, "/enum/reader/1").as(BodyCodec.string())
+            .send(context.succeeding(response -> context.verify(() -> {
+                assertEquals("one", response.body());
+                assertEquals(200, response.statusCode());
+                context.completeNow();
+            })));
+    }
+
 
     @Test
     void fromStringTest(VertxTestContext context) {
 
         client.get(PORT, HOST, "/enum/fromString/3").as(BodyCodec.string())
-                .send(context.succeeding(response -> context.verify(() -> {
-                    assertEquals("three", response.body());
-                    assertEquals(200, response.statusCode());
-                    context.completeNow();
-                })));
+            .send(context.succeeding(response -> context.verify(() -> {
+                assertEquals("three", response.body());
+                assertEquals(200, response.statusCode());
+                context.completeNow();
+            })));
     }
 }
