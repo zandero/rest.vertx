e8dba66eabbede017ab7ff32982b3d6e3361a7b7
GedMarc <marc.magon@gmail.com>
2024-10-05 14:45:12 +0200

    Add missing regex validator

--- src/main/java/com/zandero/utils/extra/ValidatingUtils.java
+++ src/main/java/com/zandero/utils/extra/ValidatingUtils.java
@@ -1,8 +1,9 @@
 package com.zandero.utils.extra;
 
-
 import com.zandero.utils.StringUtils;
 import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
 import org.apache.commons.validator.routines.UrlValidator;
 
 public final class ValidatingUtils {
@@ -15,6 +16,31 @@ public final class ValidatingUtils {
   public static boolean isUrl(String value) {
     return !StringUtils.isNullOrEmptyTrimmed(value) && UrlValidator.getInstance().isValid(value);
   }
-  
-  
+
+  /**
+   * Utility to find out if given string is a regular expression
+   * Strings without special regular expression characters are not considered regular expressions,
+   * but they technically are
+   *
+   * @param value to check if regular expression
+   * @return true if regular expression, false otherwise
+   */
+  public static boolean isRegEx(String value) {
+
+    if (StringUtils.isNullOrEmptyTrimmed(value)) {
+      return false;
+    }
+
+    if (value.chars().noneMatch(ch -> REG_EX_CHARS.indexOf(ch) > 0)) {
+      return false;
+    }
+
+    try {
+      Pattern.compile(value);
+      return true;
+    }
+    catch (PatternSyntaxException e) {
+      return false;
+    }
+  }
 }
